7cfc3bdbdc4a464f2e45ec28139234538dc614e9
Ashley Weaver <aweaver2012@gmail.com>
2015-03-26 10:36:41 -0400

    encode kind of working

--- project1.py
+++ project1.py
@@ -22,10 +22,44 @@ def replacePhrase(letter, oldPhrase, newPhrase) :
 	newLetter = letter.replace(oldPhrase, newPhrase)
 	return newLetter
 
+def encode(letter, secretPhrase) :
+	letterWords = letter.split()
+	binarySpaces = []
+	codedLetter = ""
+	secretPhraseLower = secretPhrase.lower()
+	while len(secretPhraseLower) > 0 :
+		e = secretPhraseLower[:1]
+		secretPhraseLower = secretPhraseLower[1:]
+		if e.isalpha() :
+			bin = '{0:05b}'.format(ord(e) - ord('a'))
+			print bin
+			for b in bin :
+				binList = []
+				if b == '0' :
+					binList.append(" ")
+				elif b == '1' :
+					binList.append("  ")
+			binarySpaces.append(binList)
+	
+	binarySpaces.append(["  ", "  ", "  ", "  ", "  "])
+	if len(letterWords) < (len(binarySpaces) * 5) + 1 :
+		print "Secret phrase too long for this letter."
+	else :
+		codedLetter += letterWords.pop(0)
+		for b in binarySpaces :
+			while len(b) > 0 :
+				codedLetter += b.pop(0)
+				codedLetter += letterWords.pop(0)
+		while len(letterWords) > 0 :
+			codedLetter += " "
+			codedLetter += letterWords.pop(0)
+		return codedLetter
 
-testString = "Ashley likes the sexy men"
-adjList = ["hot", "nerdy", "cute"]
-print testString
-print replaceName(testString, "Ashley", "Nick")
-print replaceWord(testString, adjList, "sexy")
-print replacePhrase(testString, "the sexy men", "hot women with tattoos")
+def decode(codedLetter) :
+	
+
+
+print encode("""This is my test letter. I need to type a lot of words here. 
+				I'm not good at typing words. Words, words, words. This is the 
+				end sentence. Goodbye, cruel world. Adding a few more words 
+				because I can't count.""", "test it")
